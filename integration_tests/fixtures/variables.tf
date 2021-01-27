3816389a5b57488c53d5dcfde485b2e473f30499
Adeel Ahmad <8452076+devops-adeel@users.noreply.github.com>
2021-01-27 13:17:34 +0000

    Feature/mvp (#1)
    * feat: mvp tf module
    
    * feat: added README file
    
    * feat: added integration tests
    
    Co-authored-by: Adeel Ahmad <adeel@hashicorp.com>

---
+++ integration_tests/fixtures/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "Google Project ID."
+  type        = string
+}
+
+variable "credentials" {
+  description = "Json credentials of gcp sa"
+  type        = string
+}
